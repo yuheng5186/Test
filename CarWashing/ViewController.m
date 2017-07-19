6cfb244b14b5209effe3f9401e29480dedd31a86
apple <2267369297@qq.com>
2017-07-19 11:27:00 +0800

    9999

--- CarWashing/ViewController.m
+++ CarWashing/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "Masonry.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,36 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    __weak typeof (self)weakSelf = self;
+
+    
+    UIView *view1 = [[UIView alloc]init];
+    view1.backgroundColor = [UIColor redColor];
+    [self.view addSubview:view1];
+    
+    UIView *view2 = [[UIView alloc]init];
+    view2.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:view2];
+    
+    
+    [view1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        
+        make.left.equalTo(weakSelf.view.mas_left).offset(0);
+        make.top.equalTo(weakSelf.view.mas_top).offset(0);
+        make.width.equalTo(weakSelf.view.mas_width).multipliedBy(0.5);
+        make.height.equalTo(weakSelf.view.mas_height).multipliedBy(0.5);
+        
+        
+    }];
+    
+    [view2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.width.and.height.equalTo(view1);
+        make.top.equalTo(view1.mas_top);
+        make.leading.equalTo(view1.mas_trailing);
+//        make.trailing.equalTo(weakSelf.view);
+
+    }];
 }
 
 
