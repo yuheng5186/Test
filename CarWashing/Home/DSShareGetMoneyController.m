1dbe3e1e4c30227b362c90238a69d9315a56f5f3
码农农 <2181003445@qq.com>
2017-08-11 14:23:27 +0800

    add weixin share page

--- CarWashing/Home/DSShareGetMoneyController.m
+++ CarWashing/Home/DSShareGetMoneyController.m
@@ -7,8 +7,20 @@
 //
 
 #import "DSShareGetMoneyController.h"
-
+#import "WeixinSessionActivity.h"
+#import "WeixinTimelineActivity.h"
+#import "HYActivityView.h"
 @interface DSShareGetMoneyController ()
+{
+    
+    NSString *title;
+    UIImage *image;
+    NSURL *url;
+    enum WXScene scene;
+    
+    NSArray *activity;
+}
+@property (nonatomic, strong) HYActivityView *activityView;
 
 @end
 
@@ -29,6 +41,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [self createSubView];
+    
+    activity = @[[[WeixinSessionActivity alloc] init], [[WeixinTimelineActivity alloc] init]];
 
 }
 
@@ -73,10 +87,65 @@
 
 - (void) getMoneyButtonClick:(id)sender {
 
+    //    UIActivityViewController *activityView = [[UIActivityViewController alloc] initWithActivityItems:@[@"这里是标题", [UIImage imageNamed:@"zensong"], [NSURL URLWithString:@"http://www.google.com"]] applicationActivities:activity];
+    //    activityView.excludedActivityTypes = @[UIActivityTypeAirDrop];
+    //    [self presentViewController:activityView animated:YES completion:nil];
+    
+    if (!self.activityView) {
+        self.activityView = [[HYActivityView alloc]initWithTitle:@"分享到" referView:self.view];
+        
+        //横屏会变成一行6个, 竖屏无法一行同时显示6个, 会自动使用默认一行4个的设置.
+        self.activityView.numberOfButtonPerLine = 2.5;
+        
+        
+        ButtonView *bv = [[ButtonView alloc]initWithText:@"微信" image:[UIImage imageNamed:@"share_platform_wechat"] handler:^(ButtonView *buttonView){
+            NSLog(@"点击微信");
+            
+            SendMessageToWXReq *req = [[SendMessageToWXReq alloc] init];
+            req.scene = scene;
+            //    req.bText = NO;
+            req.message = WXMediaMessage.message;
+            req.message.title = title;
+            [self setThumbImage:req];
+            if (url) {
+                WXWebpageObject *webObject = WXWebpageObject.object;
+                webObject.webpageUrl = [url absoluteString];
+                req.message.mediaObject = webObject;
+            } else if (image) {
+                WXImageObject *imageObject = WXImageObject.object;
+                imageObject.imageData = UIImageJPEGRepresentation(image, 1);
+                req.message.mediaObject = imageObject;
+            }
+            [WXApi sendReq:req];
+            
+        }];
+        [self.activityView addButtonView:bv];
+        
+        
+        bv = [[ButtonView alloc]initWithText:@"微信朋友圈" image:[UIImage imageNamed:@"share_platform_wechattimeline"] handler:^(ButtonView *buttonView){
+            NSLog(@"点击微信朋友圈");
+        }];
+        [self.activityView addButtonView:bv];
+        
+    }
+    
+    [self.activityView show];
 
     
 }
 
+- (void)setThumbImage:(SendMessageToWXReq *)req
+{
+    if (image) {
+        CGFloat width = 100.0f;
+        CGFloat height = image.size.height * 100.0f / image.size.width;
+        UIGraphicsBeginImageContext(CGSizeMake(width, height));
+        [image drawInRect:CGRectMake(0, 0, width, height)];
+        UIImage *scaledImage = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        [req.message setThumbImage:scaledImage];
+    }
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
