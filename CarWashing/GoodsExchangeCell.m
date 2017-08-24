22d04adfd04d93342ba22c2c9b0b27befd24ca20
apple <2267369297@qq.com>
2017-08-24 16:46:16 +0800

    新增图片  会员界面

--- CarWashing/GoodsExchangeCell.m
+++ CarWashing/GoodsExchangeCell.m
@@ -27,19 +27,19 @@
     
     UIImageView *backImgV = [[UIImageView alloc] init];
     _backImgV = backImgV;
-    backImgV.image = [UIImage imageNamed:@"tiyankaditu"];
+    backImgV.image = [UIImage imageNamed:@"bg_card"];
     [self.contentView addSubview:backImgV];
     
     UILabel *nameLab = [[UILabel alloc] init];
     _nameLab = nameLab;
-    nameLab.textColor = [UIColor colorFromHex:@"#ffffff"];
+    //nameLab.textColor = [UIColor colorFromHex:@"#ffffff"];
     nameLab.text = @"体验卡";
     nameLab.font = [UIFont systemFontOfSize:16*Main_Screen_Height/667];
     [self.contentView addSubview:nameLab];
     
     UILabel *introLab = [[UILabel alloc] init];
     _introLab = introLab;
-    introLab.textColor = [UIColor colorFromHex:@"#ffffff"];
+    //introLab.textColor = [UIColor colorFromHex:@"#ffffff"];
     introLab.text = @"商家洗车自动抵扣";
     introLab.font = [UIFont systemFontOfSize:13*Main_Screen_Height/667];
     [self.contentView addSubview:introLab];
@@ -47,7 +47,7 @@
     UILabel *scoreLab = [[UILabel alloc] init];
     _scoreLab = scoreLab;
     scoreLab.text = @"1000积分";
-    scoreLab.textColor = [UIColor colorFromHex:@"#ffffff"];
+    scoreLab.textColor = [UIColor redColor];
     scoreLab.font = [UIFont systemFontOfSize:18*Main_Screen_Height/667];
     [self.contentView addSubview:scoreLab];
     
@@ -66,8 +66,8 @@
     }];
     
     [scoreLab mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.right.equalTo(self.contentView).mas_offset(-11*Main_Screen_Height/667);
-        make.bottom.equalTo(self.contentView).mas_offset(-11*Main_Screen_Height/667);
+        make.left.equalTo(self.contentView).mas_offset(22*Main_Screen_Height/667);
+        make.bottom.equalTo(self.contentView).mas_offset(-18*Main_Screen_Height/667);
     }];
     
 }
