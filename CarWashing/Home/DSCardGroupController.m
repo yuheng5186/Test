8ba6b18052c63439fb906f3ec009a2c4b932ec45
”ymz520aa“ <2695570680@qq.com>
2017-08-28 16:00:15 +0800

    我的卡包空白页

--- CarWashing/Home/DSCardGroupController.m
+++ CarWashing/Home/DSCardGroupController.m
@@ -19,8 +19,8 @@
 #import "MBProgressHUD.h"
 #import "UdStorage.h"
 #import "CardBag.h"
-
-@interface DSCardGroupController ()<UITableViewDelegate, UITableViewDataSource>
+#import "UIScrollView+EmptyDataSet.h"//第三方空白页
+@interface DSCardGroupController ()<UITableViewDelegate, UITableViewDataSource,DZNEmptyDataSetSource,DZNEmptyDataSetDelegate>
 
 //@property (nonatomic, weak) UIView *containerView;
 //
@@ -85,7 +85,8 @@ static NSString *id_rechargeCell = @"id_rechargeCell";
         
         UITableView *rechargeView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStyleGrouped];
         _rechargeView = rechargeView;
-        
+        _rechargeView.emptyDataSetSource=self;
+        _rechargeView.emptyDataSetDelegate=self;
         [self.view addSubview:_rechargeView];
     }
     
@@ -204,7 +205,8 @@ static NSString *id_rechargeCell = @"id_rechargeCell";
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
     
-    return [_CardbagData count];
+//    return [_CardbagData count];
+    return 0;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
@@ -451,6 +453,77 @@ static NSString *id_rechargeCell = @"id_rechargeCell";
 //    }
 //}
 
+#pragma mark - 无数据占位
+//无数据占位
+- (UIImage *)imageForEmptyDataSet:(UIScrollView *)scrollView{
+    return [UIImage imageNamed:@"kabao_kongbai"];
+}
+
+- (CAAnimation *)imageAnimationForEmptyDataSet:(UIScrollView *)scrollView{
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath: @"kabao_kongbai"];
+    animation.fromValue = [NSValue valueWithCATransform3D:CATransform3DIdentity];
+    animation.toValue = [NSValue valueWithCATransform3D:CATransform3DMakeRotation(M_PI_2, 0.0, 0.0,   1.0)];
+    animation.duration = 0.25;
+    animation.cumulative = YES;
+    animation.repeatCount = MAXFLOAT;
+    return animation;
+}
+//设置文字（上图下面的文字，我这个图片默认没有这个文字的）是富文本样式，扩展性很强！
+
+//这个是设置标题文字的
+- (NSAttributedString *)titleForEmptyDataSet:(UIScrollView *)scrollView {
+    NSString *text = @"客官你还没有办理过卡";
+    
+    NSDictionary *attributes = @{NSFontAttributeName: [UIFont boldSystemFontOfSize:13.0f],
+                                 NSForegroundColorAttributeName: [UIColor colorFromHex:@"#4a4a4a"]};
+    
+    return [[NSAttributedString alloc] initWithString:text attributes:attributes];
+}
+//设置按钮的文本和按钮的背景图片
+- (NSAttributedString *)buttonTitleForEmptyDataSet:(UIScrollView *)scrollView forState:(UIControlState)state  {
+    //    NSLog(@"buttonTitleForEmptyDataSet:点击上传照片");
+    NSDictionary *attributes = @{NSFontAttributeName: [UIFont boldSystemFontOfSize:18.0f],NSForegroundColorAttributeName:[UIColor whiteColor]};
+    return [[NSAttributedString alloc] initWithString:@"去购卡" attributes:attributes];
+}
+#pragma mark-背景图片
+//-(UIImage *)buttonBackgroundImageForEmptyDataSet:(UIScrollView *)scrollView forState:(UIControlState)state{
+//
+//    return [UIImage imageNamed:@"mashangxiche-"];
+//}
+- (UIImage *)buttonImageForEmptyDataSet:(UIScrollView *)scrollView forState:(UIControlState)state {
+    return [UIImage imageNamed:@"qgouka"];
+}
+//是否显示空白页，默认YES
+- (BOOL)emptyDataSetShouldDisplay:(UIScrollView *)scrollView {
+    return YES;
+}
+//是否允许点击，默认YES
+- (BOOL)emptyDataSetShouldAllowTouch:(UIScrollView *)scrollView {
+    return NO;
+}
+//是否允许滚动，默认NO
+- (BOOL)emptyDataSetShouldAllowScroll:(UIScrollView *)scrollView {
+    return YES;
+}
+//图片是否要动画效果，默认NO
+- (BOOL) emptyDataSetShouldAllowImageViewAnimate:(UIScrollView *)scrollView {
+    return YES;
+}
+//空白页点击事件
+- (void)emptyDataSetDidTapView:(UIScrollView *)scrollView {
+    NSLog(@"空白页点击事件");
+}
+//空白页按钮点击事件
+- (void)emptyDataSetDidTapButton:(UIScrollView *)scrollView {
+    return NSLog(@"空白页按钮点击事件");
+}
+/**
+ *  调整垂直位置
+ */
+- (CGFloat)verticalOffsetForEmptyDataSet:(UIScrollView *)scrollView
+{
+    return -64.f-49;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
