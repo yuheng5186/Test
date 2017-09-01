6977d73101d790eef16c99789746394247478ad3
Mac WuXinLing <218100345@qq.com>
2017-09-01 15:40:05 +0800

    增加扫码洗车流程页面

---
+++ CarWashing/Home/DSScanPayController.h
@@ -0,0 +1,19 @@
+//
+//  DSScanPayController.h
+//  CarWashing
+//
+//  Created by Mac WuXinLing on 2017/9/1.
+//  Copyright © 2017年 DS. All rights reserved.
+//
+
+#import "BaseController.h"
+
+@interface DSScanPayController : BaseController
+
+@property(nonatomic ,copy)NSString *SerMerChant;
+@property(nonatomic ,copy)NSString *SerProject;
+
+@property(nonatomic ,copy)NSString *Jprice;
+@property(nonatomic ,copy)NSString *Xprice;
+
+@end
