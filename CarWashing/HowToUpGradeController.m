2b5dd73f86e64274787f4782007ac2066e22c955
”ymz520aa“ <2695570680@qq.com>
2017-09-07 16:30:02 +0800

    修改slider悬浮uilabel的内边距

--- CarWashing/HowToUpGradeController.m
+++ CarWashing/HowToUpGradeController.m
@@ -99,12 +99,18 @@ static NSString *id_wayToUpCell = @"id_wayToUpCell";
     
     slider.currentValueColor = [UIColor colorFromHex:@"#febb02"];
     slider.maxValue = [self.NextLevelScore integerValue];
-    slider.currentSliderValue = [self.CurrentScore integerValue];
+    slider.currentSliderValue =[self.CurrentScore integerValue];
     slider.showTextColor = [UIColor colorFromHex:@"#febb02"];
     slider.showTouchView = YES;
     slider.showScrollTextView = YES;
     slider.touchViewColor = [UIColor colorFromHex:@"#febb02"];
     slider.delegate = self;
+#pragma mark-重至slider的imageview宽度
+    CGSize imageWidth=[slider.scrollShowTextLabel boundingRectWithSize:CGSizeMake(10000, 17*Main_Screen_Height/667)];
+    
+    slider.scrollShowTextLabel.frame=CGRectMake(5*Main_Screen_Width/375, 2*Main_Screen_Height/667, imageWidth.width, 17*Main_Screen_Height/667);
+    slider.imageView.frame=CGRectMake(0, -2*Main_Screen_Height/667,imageWidth.width+10*Main_Screen_Height/667,25*Main_Screen_Height/667);
+    
     [self.view addSubview:slider];
     UILabel *maxLab = [[UILabel alloc] init];
     
