34292d7c5c043b875fea77e7d8e2193be05320db
apple <2695570680@qq.com>
2017-09-29 15:11:34 +0800

    杨朝阳

--- Tool/AFNetwork/HTTPDefine.h
+++ Tool/AFNetwork/HTTPDefine.h
@@ -6,14 +6,14 @@
 //
 //#define kHTTPImg @"http://192.168.3.80:8090"
 
-#pragma mark-服务器
-#define Khttp @"http://api.qiangweilovecar.com/api/"
-
-#define kHTTPImg @"http://api.qiangweilovecar.com"
-//#pragma mark-本地
-//#define Khttp @"http://192.168.2.152:8090/api/"
+//#pragma mark-服务器
+//#define Khttp @"http://api.qiangweilovecar.com/api/"
 //
-//#define kHTTPImg @"http://192.168.2.152:8090"
+//#define kHTTPImg @"http://api.qiangweilovecar.com"
+#pragma mark-本地
+#define Khttp @"http://192.168.2.152:8090/api/"
+
+#define kHTTPImg @"http://192.168.2.152:8090"
 
 //#define Khttp @"http://192.168.3.101:8090/api/"
 //
