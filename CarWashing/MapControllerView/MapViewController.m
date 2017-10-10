8291de1b7886f57062170e1232a5c4cac0cd0a83
apple <2695570680@qq.com>
2017-10-10 18:32:20 +0800

    1

--- CarWashing/MapControllerView/MapViewController.m
+++ CarWashing/MapControllerView/MapViewController.m
@@ -25,12 +25,12 @@
 }
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.contentView.backgroundColor=[UIColor lightGrayColor];
+    self.contentView.backgroundColor= [UIColor colorFromHex:@"f6f6f6"];
     UIView *titleView                  = [UIUtil drawLineInView:self.contentView frame:CGRectMake(0, 0, Main_Screen_Width, 64) color:[UIColor clearColor]];
     titleView.top                      = 0;
     UIButton * rightBtn = [UIButton buttonWithType:UIButtonTypeCustom];
     rightBtn.frame = CGRectMake(0, 8, 70, 64);
-    [rightBtn setImage:[UIImage imageNamed:@"dingwei2"] forState:UIControlStateNormal];
+    [rightBtn setImage:[UIImage imageNamed:@"ditu_fanhui"] forState:UIControlStateNormal];
     [rightBtn addTarget:self action:@selector(rightBtnClick) forControlEvents:UIControlEventTouchUpInside];
     [titleView addSubview:rightBtn];
 }
