faab4c54eab0c80ddf530442a0000bda3223184e
apple <2695570680@qq.com>
2017-11-16 16:50:14 +0800

    1221

--- CarWashing/UserInfo/DSSettingController.m
+++ CarWashing/UserInfo/DSSettingController.m
@@ -262,16 +262,10 @@
                 [defaults removeObjectForKey:@"Insure"];
                 [defaults synchronize];
             
-        
-            
                 LoginViewController *loginControl = [[LoginViewController alloc]init];
                 UINavigationController *nav         = [[UINavigationController alloc]initWithRootViewController:loginControl];
                 nav.navigationBar.hidden      = YES;
-            
-            
-            
                 self.view.window.rootViewController      = nav;
-
 
             
         }
@@ -287,22 +281,6 @@
 
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
-
-
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
