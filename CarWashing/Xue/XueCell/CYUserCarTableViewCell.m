fe88b6241e9539669e095a0d6e72a269fd876845
apple <2695570680@qq.com>
2017-11-17 14:00:20 +0800

    修改bug

--- CarWashing/Xue/XueCell/CYUserCarTableViewCell.m
+++ CarWashing/Xue/XueCell/CYUserCarTableViewCell.m
@@ -87,7 +87,7 @@
 {
     [self.carImageView sd_setImageWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"%@%@",kHTTPImg,model.Img]] placeholderImage:[UIImage imageNamed:@"photo"]];
     self.distancelabel.text= [NSString stringWithFormat:@"%@公里",model.Mileage];
-    self.timeLabel.text= [NSString stringWithFormat:@"%@",model.Manufacture];
+    self.timeLabel.text= [NSString stringWithFormat:@"%@",model.ActDate];
     self.titlelabel.text= [NSString stringWithFormat:@"%@%@",model.CarBrand,model.CarType];
     self.detaillabel.text= [NSString stringWithFormat:@"%@",model.CarComment];
 }
